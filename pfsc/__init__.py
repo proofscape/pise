c45529e951640414f6be168f48d08c4fb3258af4
Steve Kieffer <sk@skieffer.info>
2022-11-22 17:21:58 -0500

    Bump dev version

--- pfsc/__init__.py
+++ pfsc/__init__.py
@@ -29,7 +29,7 @@ from pfsc.constants import REDIS_CHANNEL, ISE_PREFIX
 from pfsc.excep import PfscExcep, PECode
 from config import config_lookup, ProductionConfig
 
-__version__ = '0.25.1'
+__version__ = '0.25.2-dev'
 
 socketio = SocketIO()
 pfsc_cli = AppGroup('pfsc')
