37e82c08660599b17760cca3ef94a4c9d8e8c57c
Steve Kieffer <sk@skieffer.info>
2022-11-30 15:23:12 -0500

    manage/se: catch stale reference exceps

--- manage/tests/selenium/test_basic_run_01.py
+++ manage/tests/selenium/test_basic_run_01.py
@@ -18,6 +18,7 @@ import time
 import logging
 
 from tests.selenium.util import Tester
+import selenium.common.exceptions
 
 
 class TestBasicRun01(Tester):
@@ -47,8 +48,6 @@ class TestBasicRun01(Tester):
 
         self.open_repo(repopath, s_repo_root_node, select_tab='fs')
 
-        prog_bar = self.find_element(s_prog_bar)
-
         # Open context menu and click "build recursive" option.
         self.click_nth_context_menu_option(s_repo_root_node, "dijit_Menu_1", 7, "Build Recursive")
         t0 = time.time()
@@ -60,10 +59,21 @@ class TestBasicRun01(Tester):
         logger.info("Sampling prog bar width for 3 seconds...")
         widths = set()
         for i in range(30):
-            w_str = prog_bar.value_of_css_property('width')
-            w_flt = float(w_str.replace('px', ''))
-            logger.debug(f'Prog bar width: {w_flt:.2f}')
-            widths.add(w_flt)
+            # It seems the progress bar element is sometimes recreated during
+            # the display of progress. Just in case that happens between the
+            # time we grab the element and the time we try to read its width
+            # property, we catch `StaleElementReferenceException`, and simply
+            # pass. Since we're taking 30 samples, in most cases we'll get
+            # plenty of good ones.
+            try:
+                prog_bar = self.find_element(s_prog_bar)
+                w_str = prog_bar.value_of_css_property('width')
+            except selenium.common.exceptions.StaleElementReferenceException:
+                pass
+            else:
+                w_flt = float(w_str.replace('px', ''))
+                logger.debug(f'Prog bar width: {w_flt:.2f}')
+                widths.add(w_flt)
             time.sleep(0.1)
         # Probably got 20 or more different values,
         # but we'll just look for two distinct non-zero values.
