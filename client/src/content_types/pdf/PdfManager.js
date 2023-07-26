fb6ad21132d3a026862ded7d8da620927cfcd7c7
Steve Kieffer <sk@skieffer.info>
2023-07-26 16:47:09 -0400

    Cover sphinx content type in `PdfManager.handleHighlightMouseEvent()`

--- client/src/content_types/pdf/PdfManager.js
+++ client/src/content_types/pdf/PdfManager.js
@@ -887,8 +887,8 @@ var PdfManager = declare(AbstractContentManager, {
                 } else {
                     cdo.select = siid;
                 }
-            } else if (stype === "NOTES") {
-                // For highlights supplied by a notes page, the siid's are the widget uids
+            } else if (stype === "NOTES" || stype === "SPHINX") {
+                // For highlights supplied by an anno or sphinx page, the siid's are the widget uids
                 // of the widgets that supplied them.
                 const selector = `.${siid}`;
                 cdo.select = selector;
