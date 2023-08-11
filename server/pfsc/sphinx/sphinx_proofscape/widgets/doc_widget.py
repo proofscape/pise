c8aa27b240707555bf690a241f118daf707d0880
Steve Kieffer <sk@skieffer.info>
2023-08-11 18:19:51 -0400

    Uniformize license headers

--- server/pfsc/sphinx/sphinx_proofscape/widgets/doc_widget.py
+++ server/pfsc/sphinx/sphinx_proofscape/widgets/doc_widget.py
@@ -1,7 +1,5 @@
 # --------------------------------------------------------------------------- #
-#   Sphinx-Proofscape                                                         #
-#                                                                             #
-#   Copyright (c) 2022-2023 Proofscape contributors                           #
+#   Copyright (c) 2011-2023 Proofscape Contributors                           #
 #                                                                             #
 #   Licensed under the Apache License, Version 2.0 (the "License");           #
 #   you may not use this file except in compliance with the License.          #
